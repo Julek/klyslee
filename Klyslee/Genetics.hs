295481b1e436a8910fcc0e7b18db3d92ae589147
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2011-12-30 09:37:48 +0000

    Some standard helper functions to make GPGA

--- Klyslee/Genetics.hs
+++ Klyslee/Genetics.hs
@@ -1,1 +1,23 @@
 module Klyslee.Genetics where
+
+import System.Random
+import Klyslee.Utils
+
+mutation = 0.03
+
+class Breadable a where
+  leSexyTime :: a -> a -> a
+  mutate :: a -> a
+  getRandCitizen :: IO a
+
+instance Breadable Int where
+  leSexyTime a b = a
+  mutate a = a
+  getRandCitizen = randomIO :: IO Int
+
+getPopulation :: (Breadable a) => Int -> IO [a]
+getPopulation 0 = return []
+getPopulation a = (getRandCitizen >>= (\x -> (getPopulation (a-1)) >>= (\y -> return (x:y))))
+
+
+-- ga :: ()-> () -> 
