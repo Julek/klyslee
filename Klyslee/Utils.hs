2bd0cd8b55caa33ab3db2c2562d5ebb34d2dbc67
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-01-21 19:35:03 +0000

    Removed old comments

--- Klyslee/Utils.hs
+++ Klyslee/Utils.hs
@@ -26,27 +26,3 @@ xchgBits a1 a2 n = (setBitTo a1 n (testBit a2 n), setBitTo a2 n (testBit a1 n))
 setBitTo :: (Bits a) => a -> Int -> Bool -> a
 setBitTo a n True = setBit a n
 setBitTo a n False = clearBit a n
-  
-{-
-import Debug.Trace
-import System.Random
-
-randSublist :: Int -> IO [a] -> IO [a]
-randSublist 0 list = return []
-randSublist n list = rest >>= (\b -> randMember >>= (\f -> return $ f:b))
-  where randMember = list >>= (\ls -> (randomIO :: IO Int) >>= (\i -> return $ ls !! (i `mod` (length ls))))
-        rest = randSublist (n-1) list
-
-coalesceIO :: [IO a] -> IO [a]
-coalesceIO [] = return []
-coalesceIO (l:ls) = l >>= (\f -> (coalesceIO ls) >>= (\b -> return $ f:b))
-
-getRandPercentage :: IO Float
-getRandPercentage = do
-  r <- (randomIO :: IO Int)
-  return $ fromIntegral (r `mod` 100) / 100
-
-repeatTimes :: (a -> a) -> a -> Int -> [a]
-repeatTimes f arg 0 = []
-repeatTimes f arg n = new:(repeatTimes f arg (n-1))
-  where new = f arg-}
