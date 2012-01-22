d8dc51fd6984ba354b7e38d069dd5c7dbfe05c5d
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-01-22 15:20:11 +0000

    Added fitness function, a bit ugly though

--- Klyslee/Notes.hs
+++ Klyslee/Notes.hs
@@ -4,31 +4,37 @@ import Klyslee.Breedable
 import Klyslee.GeneticsVars
 import Klyslee.Utils
 
+import Data.List
+import Data.Maybe
 import System.Random
 import Control.Monad.State
 
 tunel = 4 :: Int
 
+intervals = [((Do, Norm), [(Mi, Norm), (Mi, Flat), (Sol, Norm)]), ((Re, Flat), [(Fa, Norm), (Fa, Flat), (La, Flat)]), ((Re, Norm), [(Sol, Flat), (Fa, Norm), (La, Norm)]), ((Mi, Flat), [(Sol, Norm), (Sol, Flat), (Si, Flat)]), ((Mi, Norm), [(La, Flat), (Sol, Norm), (Si, Norm)]), ((Fa, Norm), [(La, Norm), (La, Flat), (Do, Norm)]), ((Sol, Flat), [(Si, Flat), (La, Norm), (Re, Flat)]), ((Sol, Norm), [(Si, Norm), (Si, Flat), (Re, Norm)]), ((La, Flat), [(Do, Norm), (Si, Norm), (Mi, Flat)]), ((La, Norm), [(Re, Flat), (Do, Norm), (Mi, Norm)]), ((Si, Flat), [(Re, Norm), (Re, Flat), (Fa, Norm)]), ((Si, Norm), [(Mi, Flat), (Re, Norm), (Sol, Flat)])]
+
 newtype Melody = Melody [Note]
 
 data Note = Note Octave AbsNote Intonation
-
+            deriving(Eq)
+                    
 type Octave = Int
 
 data AbsNote = Do | Re | Mi | Fa | Sol | La | Si
-                                              deriving(Show)
+                                              deriving(Show, Eq)
 
-data Intonation = Norm | Sharp | Flat
+data Intonation = Norm | Flat
+                         deriving(Eq)
 
 
 instance Breedable Melody where
   genRand = replicateM tunel (do
     oct <- doState (randomR (1, 8))
     n <-doState (randomR (0, 6))
-    i <- doState (randomR (-1, 1))
+    i <- doState (randomR (-1, 0))
     let note = getNote n
         int = getIntonation i
-    return (Note oct note int)) >>= (return . Melody)
+    return (standardise $ Note oct note int)) >>= (return . Melody)
 
   leSexyTime (Melody m) (Melody f) = foldM (\r i -> do
                               choice <- (doState  random) :: (RandomGen g, MonadState g m) => m Float
@@ -56,13 +62,11 @@ instance Show Note where
 
 instance Show Intonation where
   show Norm = ""
-  show Sharp = "#"
   show Flat = "♭"
 
 getIntonation :: Int -> Intonation
 getIntonation  (-1) = Flat
 getIntonation 0 = Norm
-getIntonation 1 = Sharp
 
 getNote :: Int -> AbsNote
 getNote 0 = Do
@@ -72,3 +76,28 @@ getNote 3 = Fa
 getNote 4 = Sol
 getNote 5 = La
 getNote 6 = Si
+
+standardise :: Note -> Note
+standardise (Note oct Fa Flat) = Note oct Mi Norm
+standardise (Note oct Do Flat) = Note oct Si Norm
+standardise x = x
+
+song_fitness :: Melody -> Double
+song_fitness (Melody ls) = sum $ unfoldr interval_fitness_wrapper ls
+
+interval_fitness :: Note -> Note -> Double
+interval_fitness (Note oct1 note1 int1) (Note oct2 note2 int2) = interval_penalty + octave_penalty
+  where interval_penalty = if(elem (note2, int2) $ fromJust $ lookup (note1, int1) intervals)
+                           then
+                             0 + octave_penalty
+                           else
+                             1
+        octave_penalty = if(abs(oct1 - oct2) < 2)
+                         then
+                           0
+                         else
+                           1
+
+interval_fitness_wrapper :: [Note] -> Maybe (Double, [Note])
+interval_fitness_wrapper (n1:n2:ns) = Just (interval_fitness n1 n2, n2:ns)
+interval_fitness_wrapper x = Nothing
